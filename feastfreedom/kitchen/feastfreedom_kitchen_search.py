5d82595e698475788990ca880ab945f9d777c25d
Ubuntu <ubuntu@ip-172-31-94-92.ec2.internal>
2021-06-25 22:22:26 +0000

    added config info for selenium; need to resolve db

--- feastfreedom/kitchen/feastfreedom_kitchen_search.py
+++ feastfreedom/kitchen/feastfreedom_kitchen_search.py
@@ -9,7 +9,7 @@ class FeastFreedomHomeSearch(unittest.TestCase):
 
     def test_search_in_feastfreedom(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/")
+        driver.get("http://100.24.4.172/")
         self.assertIn("FeastFreedom Home Page", driver.title)
         #TODO: read DOCS: https://selenium-python.readthedocs.io/getting-started.html#simple-usage
         # elem = driver.find_element_by_name("q")
@@ -23,7 +23,7 @@ class FeastFreedomKitchenSearch(unittest.TestCase):
 
     def test_search_in_feastfreedom_kitchen(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/kitchen")
+        driver.get("http://0.0.0.0:8000/kitchen")
         self.assertIn("Kitchen App List Page", driver.title)
         #TODO: read DOCS: https://selenium-python.readthedocs.io/getting-started.html#simple-usage
         # elem = driver.find_element_by_name("q")
@@ -33,25 +33,29 @@ class FeastFreedomKitchenSearch(unittest.TestCase):
 
     def test_search_in_feastfreedom_kitchen_detail(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/kitchen/1")
+        #driver.get("http://100.24.4.172/kitchen/1")
+        driver.get("http://0.0.0.0:8000/kitchen/1")
         self.assertIn("Kitchen App Detail Page", driver.title)
         assert "No results found." not in driver.page_source
 
     def test_search_in_feastfreedom_kitchen_create(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/kitchen/create")
+        #driver.get("http://100.24.4.172/kitchen/create")
+        driver.get("http://0.0.0.0:8000/kitchen/create")
         self.assertIn("Kitchen App Create Page", driver.title)
         assert "No results found." not in driver.page_source
 
     def test_search_in_feastfreedom_kitchen_update(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/kitchen/1/update")
+        #driver.get("http://100.24.4.172/kitchen/1/update")
+        driver.get("http://0.0.0.0:8000/kitchen/1/update")
         self.assertIn("Kitchen App Update Page", driver.title)
         assert "No results found." not in driver.page_source
 
     def test_search_in_feastfreedom_kitchen_delete(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/kitchen/1/delete")
+        #driver.get("http://100.24.4.172/kitchen/1/delete")
+        driver.get("http://0.0.0.0:8000/kitchen/1/delete")
         self.assertIn("Kitchen App Delete Page", driver.title)
         assert "No results found." not in driver.page_source
 
